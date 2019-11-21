5d26a3262762084f735722d208853c2e477a20d0
helloalexdodd <alexdodd@gmx.com>
2019-11-21 11:06:54 -0500

    Updates

---
+++ precache-manifest.930d96869891064642b0c352760c83ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4270635c461bdd4f931ecd24cd35757c",
+    "url": "/boggle/index.html"
+  },
+  {
+    "revision": "abd1b564ef02ba624831",
+    "url": "/boggle/static/js/2.189593f1.chunk.js"
+  },
+  {
+    "revision": "11b5f539dbdfc4e7bfe9",
+    "url": "/boggle/static/js/main.2666ce15.chunk.js"
+  },
+  {
+    "revision": "49acabd4bc07e4c28b10",
+    "url": "/boggle/static/js/runtime~main.8f21e824.js"
+  },
+  {
+    "revision": "24485b4e304cd2422b727041d801b274",
+    "url": "/boggle/static/media/logo.24485b4e.png"
+  }
+]);
